529aa47ed21ddb6a66a578c7be265eedb84ca687
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 17:41:56 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="kk">
+    <h1>Hello!! I am the parent....</h1>
+    <h2>Name is: {{childname}}</h2>
+    <Child :childname="childname" @pleasechange="childname = $event"></Child>
+    <button @click="changename">Change in parent and child</button>
+  </div>
+</template>
+
+<script>
+import child from './child';
+export default {
+  components:{
+    Child : child
+  },
+  data: function(){
+    return{
+      childname : "kartik"
+    }
+  },
+  methods:{
+    changename(){
+      this.childname="Aishwary";
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+  div{
+    margin: 3rem;
+  }
+  .kk{
+    background-color: cornsilk;
+    padding: 3rem;
+  }
+</style>>
+
+</style>
---
+++ src/child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Hello!! I am the child....</h1>
+    <h2>my name is {{childname}} .</h2>
+    <button @click="changehere">Change name in child</button>
+    <button @click="changeeverywhere">Change name in both paremt and child ($emit is used) </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["childname"],
+  methods:{
+      changehere(){
+          this.childname = "yash";
+      },
+      changeeverywhere(){
+          this.$emit("pleasechange","chirag");
+      }
+  }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+    }
+    div{
+        background-color: burlywood;
+        padding: 2rem;
+    }
+</style>>
+
